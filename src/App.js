cb0e8ded38b4c058ab0d0c4f0807abef6d51882e
Dylan <dfryed@gmail.com>
2018-05-28 10:51:15 -0500

    Changed doodle, added new pages and routing.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import About from './About';
+import Contact from './Contact';
 import Header from './Header';
+import Main from './Main';
 import NoPage from './NoPage';
+import Portfolio from './Portfolio';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="index">
+      <div>
         <NoPage />
+        {/* <Route exact path='/' render={() => {
+          <div className="index">
+            <Main />
+          </div>;
+        }}/>
+
+        <Route path='/About' render={() => {
+          <div className="index">
+            <About />
+          </div>;
+        }}/>
+
+        <Route path='/Contact' render={() => {
+          <div className="index">
+            <Contact />
+          </div>;
+        }}/>
+
+        <Route path='/Portfolio' render={() => {
+          <div className="index">
+            <Portfolio />
+          </div>;
+        }}/> */}
+
       </div>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
       <div>
-        
+        <ul>
+          <li><Link to='/Resume'>Resume</Link></li>
+          <li><Link to='/About'>About</Link></li>
+          <li><Link to='/Contact'>Contact</Link></li>
+        </ul>
       </div>
     );
   }
