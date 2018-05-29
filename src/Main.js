22ac826b197614728c716d17de819d205144a086
Dylan <dfryed@gmail.com>
2018-05-28 20:43:04 -0500

    Updated text

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,7 @@ class Main extends Component {
   render() {
     return (
         <div>
-          <h1>Dylan Frye.</h1>
+          <h1>I'm Dylan Frye.</h1>
           <p>
           Crafting things is my passion. I love challenges
           and those impossible-at-first-glance problems. Give me critiques and feedback
