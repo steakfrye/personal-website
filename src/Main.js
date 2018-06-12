c8e4a199cca982bad978db90c43c46dc3c1397e9
Dylan <dfryed@gmail.com>
2018-06-12 17:20:35 -0500

    Removed bulk of CSS. Added dropdown transition.

--- src/Main.js
+++ src/Main.js
@@ -6,9 +6,9 @@ class Main extends Component {
         <div className='home'>
           <h1>I'm Dylan Frye.</h1>
           <p>
-          Crafting things is my passion. I love challenges
+          Crafting is my passion. I love challenges
           and those impossible-at-first-glance problems. Give me critiques and feedback
-          so I can build the best I can.
+          so I can build your project the best I can.
           </p>
         </div>
     );
