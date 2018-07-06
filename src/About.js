16f8f2a401885aea0b47d5667a218c584103d913
Dylan <dfryed@gmail.com>
2018-07-06 13:56:55 -0500

    Added SVG instead of photo.

--- src/About.js
+++ src/About.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import Icon from './Icon';
 
 class About extends Component {
   render() {
     return (
       <div className='about'>
         <p className='bio'>
-          <img className='img-circle' src={require('./files/IMG_2905.jpg')} alt={'dylan-portrait'}/>
           When I'm not coding, I'm probably spending
           time with my wife and son (with another on the way!). I do a lot
           of cooking, even making my own butter and nursing my sourdough starter.
           I pride myself in my culinary abilities, and I like to educate myself in
           an array of crafts and keep my skills sharp.
+          <Icon />
         </p>
       </div>
     );
