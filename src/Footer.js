e0bb90b673eb4e9c407797b8fa2b3926bb3fa3b4
Dylan <dfryed@gmail.com>
2018-07-08 13:55:05 -0500

    Fixed footer. Like, it's literally fixed in place

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,8 @@ import React, {Component} from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div className='footer'>
-        <p>&copy; 2018 Dylan Frye</p>
+      <div>
+        <h5 className='footer'>&copy; 2018 Dylan Frye</h5>
       </div>
     );
   }
