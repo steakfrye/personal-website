c92664dba3fc92afba4375b5d6026b28d648759b
Dylan <dfryed@gmail.com>
2018-07-09 10:06:08 -0500

    icon added to git

---
+++ src/Icon.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Icon extends Component {
+  render() {
+    return (
+      <div>
+        <svg className="icon" width="200" height="240" xmlns="http://www.w3.org/2000/svg">
+          <rect ry="62" rx="62" id="svg_1" height="194" width="162" y="33" x="13" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="none"/>
+          <rect ry="13" rx="13" fill-opacity="0" id="svg_8" height="35" width="57" y="104" x="28" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="none"/>
+          <rect id="svg_11" ry="13" rx="13" fill-opacity="0" height="35" width="57" y="105.5" x="107.5" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="none"/>
+          <line id="svg_13" y2="123" x2="109" y1="123" x1="86" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="none"/>
+          <polygon fill="none" id="svg_31"/>
+          <path stroke="#000000" id="svg_44" d="m48,172.51596l96,0.48404c0,0 -15.13044,-14.52112 -53.21739,-14.03708c-38.08696,0.48404 -42.78261,13.55304 -42.78261,13.55304z" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" fill="#000000"/>
+          <path fill="#7f3f00" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" stroke-opacity="0" d="m10.88818,88.87293c-3.93814,-19.86898 -15.75256,-40.74823 3.93814,-56.57606l38.39686,1.01029c20.01887,-13.80725 20.34706,-34.68651 74.82465,-29.29831c20.34706,10.10286 87.95179,44.45261 60.05663,31.31888c-5.57903,22.22631 26.91062,61.62749 -15.75256,65.66864c-42.66317,4.04114 -98.12531,-27.27774 -107.3143,-42.43204c-9.18899,-15.1543 -36.09962,20.20573 -54.14942,30.3086z" id="svg_2" stroke="#000000"/>
+          <path fill="#000000" stroke-width="5" stroke-dasharray="null" stroke-linejoin="null" stroke-linecap="null" d="m13.46865,145.5c16.79597,20.16667 51.54058,64.66667 51.86991,63.5c0.32932,-1.16667 59.60922,-1 61.74989,-0.5l47.91791,-55c1.31733,21.66667 4.61066,57.33333 -55.3279,73.5l-54.8339,0c-57.13923,-11.66667 -57.46857,-69.33333 -51.37591,-81.5l0,0z" id="svg_7" stroke="#000000"/>
+        </svg>
+        {/* <svg xmlns="http://www.w3.org/2000/svg" version="1.2" baseProfile="tiny" x="0px" y="0px" viewBox="0 0 100 100">
+          <g>
+            <polygon fill-rule="evenodd" points="79.552,36.363 79.552,31.818 70.461,31.818 70.461,36.363 70.461,50 75.006,50 75.006,36.363 79.545,36.363 79.545,40.909 84.09,40.909 84.09,36.363"></polygon>
+            <rect x="34.101" y="0" fill-rule="evenodd" width="9.091" height="4.545"></rect>
+            <rect x="20.445" y="59.09" fill-rule="evenodd" width="4.546" height="9.091"></rect>
+            <polygon fill-rule="evenodd" points="34.082,86.363 34.082,77.272 29.537,77.272 29.537,68.181 24.991,68.181 24.991,77.272 29.536,77.272 29.536,86.363"></polygon>
+            <polygon fill-rule="evenodd" points="79.545,86.363 75.006,86.363 75.006,95.454 38.646,95.454 38.646,86.363 34.101,86.363 34.101,95.454 34.082,95.454 34.082,100 34.101,100 38.646,100 75.006,100 79.537,100 79.552,100 79.552,86.363 84.09,86.363 84.09,72.727 79.545,72.727"></polygon>
+            <polygon fill-rule="evenodd" points="15.909,45.454 24.982,45.454 24.982,40.909 15.909,40.909 15.891,40.909 11.364,40.909 11.364,54.545 15.891,54.545 15.891,59.09 20.436,59.09 20.436,54.545 15.909,54.545  "></polygon>
+            <polygon fill-rule="evenodd" points="56.799,45.454 61.345,45.454 61.345,31.818 70.455,31.818 70.455,27.272 56.819,27.272 56.819,31.818 56.799,31.818"></polygon>
+            <polygon fill-rule="evenodd" points="47.746,27.272 56.799,27.272 56.799,22.727 47.746,22.727 47.746,4.545 43.201,4.545 43.201,45.454 47.746,45.454"></polygon>
+            <polygon fill-rule="evenodd" points="29.537,54.545 34.082,54.545 34.082,4.545 29.537,4.545 29.537,45.454 24.991,45.454 24.991,50 29.537,50"></polygon>
+            <rect x="84.09" y="40.909" fill-rule="evenodd" width="4.547" height="31.818"></rect>
+          </g>
+        </svg> */}
+      </div>
+    );
+  }
+};
+
+export default Icon;
